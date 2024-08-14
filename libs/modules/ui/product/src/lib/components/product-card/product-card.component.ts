4f773e7ef9b8f9b63ec043fdc387247751607770
Vanessa Ortega <ortegavan@live.com>
2024-08-14 08:25:53 -0300

    feat: redesign

--- libs/modules/ui/product/src/lib/components/product-card/product-card.component.ts
+++ libs/modules/ui/product/src/lib/components/product-card/product-card.component.ts
@@ -1,15 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
 import { Product } from '@ecommerce/product-data-access';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'ecommerce-product-card',
     standalone: true,
-    imports: [CommonModule, MatCardModule],
+    imports: [CommonModule, MatButtonModule],
     templateUrl: './product-card.component.html',
     styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
     @Input() product!: Product;
+    @Input() showAddToCart = true;
+    @Output() addToCart = new EventEmitter<Product>();
+
+    onAddToCart(product: Product, event: MouseEvent) {
+        event.stopPropagation();
+        this.addToCart.emit(product);
+    }
 }
