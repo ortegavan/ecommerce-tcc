aca13e67496e297d59613c572d9c08deceaaef2c
Agemiro Alves de Sousa Neto <agemiroalves11@gmail.com>
2024-08-17 03:59:59 +0200

    test: add e2e tests

--- apps/ecommerce-admin-e2e/src/e2e/app.cy.ts
+++ apps/ecommerce-admin-e2e/src/e2e/app.cy.ts
@@ -3,11 +3,11 @@ import { getGreeting } from '../support/app.po';
 describe('ecommerce-admin-e2e', () => {
     beforeEach(() => cy.visit('/'));
 
-    it('should display welcome message', () => {
+    it('should display e-Commerce Admin message', () => {
         // Custom command example, see `../support/commands.ts` file
         cy.login('my-email@something.com', 'myPassword');
 
         // Function helper example, see `../support/app.po.ts` file
-        getGreeting().contains(/Welcome/);
+        getGreeting().contains(/e-Commerce Admin/);
     });
 });
