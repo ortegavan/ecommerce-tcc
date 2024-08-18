a94d7e7abf66101301874dc15595641a4a04de7e
Van Ortega <ortegavan@live.com>
2024-08-18 11:53:58 -0300

    test: novos e2e

--- apps/ecommerce-admin-e2e/src/e2e/user-list.cy.ts
+++ apps/ecommerce-admin-e2e/src/e2e/user-list.cy.ts
@@ -1,8 +1,18 @@
 describe('user list page', () => {
     beforeEach(() => cy.visit('/'));
 
-    it('should show the page with the list of registered users', () => {
+    it('should show the header of the page', () => {
         cy.get('h1').contains('e-Commerce Admin');
         cy.get('h2').contains('Usuários cadastrados');
+    });
+
+    it('should show the list of users', () => {
+        cy.get('table').find('tr').should('have.length.greaterThan', 1);
+    });
+
+    it('should show the details of a user', () => {
+        cy.get('button').first().click();
+        cy.get('.user-details__avatar').should('exist');
+        cy.get('.user-details__close').click();
     });
 });
