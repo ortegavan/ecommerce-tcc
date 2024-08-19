211894fb346011bc283b8c98164fee06d8e85933
Van Ortega <ortegavan@live.com>
2024-08-19 11:21:45 -0300

    feat(admin): implementada autenticação

--- libs/modules/data-access/auth/src/lib/guards/login/login.guard.ts
+++ libs/modules/data-access/auth/src/lib/guards/login/login.guard.ts
@@ -1,15 +1,15 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { map } from 'rxjs';
-import { AuthService } from '../auth/auth.service';
+import { AuthService } from '../../auth/auth.service';
 
-export function authGuard() {
+export function loginGuard() {
     return () => {
         const router = inject(Router);
         const email$ = inject(AuthService).email$;
 
         return email$.pipe(
-            map((email) => (email ? router.createUrlTree(['/']) : true))
+            map((email) => (email ? true : router.createUrlTree(['/', 'auth'])))
         );
     };
 }
